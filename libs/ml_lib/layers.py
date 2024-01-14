f307dbf432aeb53be812426fee26bef28a3defe9
Zhong <zhongsheng2002@foxmail.com>
2024-01-14 20:11:44 +0800

    Complete 50% Loadv2

--- libs/ml_lib/layers.py
+++ libs/ml_lib/layers.py
@@ -67,19 +67,11 @@ class SoftmaxWithLoss:
 
 
 class Conv:
-    def __init__(self, W = None, b = None,
-                 input_channel = None, output_channel = None, kernel_size=3,
+    def __init__(self, W, b,
                  stride=1, pad=0, optimizer=SGD(),
                  quant_mode=QuantMode.FullPrecision):
-        if W and b: 
-            self.W = W
-            self.b = b
-        elif input_channel and output_channel: 
-            self.W = mypy.random.normal(loc=0, scale=0.01, size=(output_channel, input_channel, kernel_size, kernel_size))
-            # self.W = mypy.zeros([output_channel, input_channel, kernel_size, kernel_size], dtype=float)
-            self.b = mypy.zeros(output_channel, dtype=float)
-        else:
-            print("卷积核未初始化")
+        self.W = W
+        self.b = b
 
         self.stride = stride
         self.pad = pad
@@ -138,14 +130,11 @@ class Conv:
         N_W, C, H_W, W_W = self.W.shape
         # dout: (N, N_W, out_h, out_w) -> (N*out_h*out_w, N_W)
         dout = dout.transpose(0, 2, 3, 1).reshape(-1, N_W)
-        db = mypy.sum(dout, axis=0)
+        self.db = mypy.sum(dout, axis=0)
 
         # 全精度，不进行量化
         if self.quant_mode == QuantMode.FullPrecision:
-            dW = mypy.dot(dout.T, self.col).reshape(N_W, C, H_W, W_W)
-
-            self.optimizer.update(self.W, dW)
-            self.optimizer.update(self.b, db)
+            self.dW = mypy.dot(dout.T, self.col).reshape(N_W, C, H_W, W_W)
 
             dcol = mypy.dot(dout, self.col_W.T)
             dx = col2im(dcol, self.x.shape, H_W, W_W, self.stride, self.pad)
@@ -157,9 +146,7 @@ class Conv:
             dout_int8 = self.quantizer.quantizeDOUTSymm(dout)
 
             dW_int = mypy.dot(dout_int8.T, col_int8).reshape(N_W, C, H_W, W_W) #
-            dW_q = self.quantizer.dequantize_dW(dW_int)
-            self.optimizer.update(self.W, dW_q)
-            self.optimizer.update(self.b, db)
+            self.dW = self.quantizer.dequantize_dW(dW_int)
 
             dcol_int = mypy.dot(dout_int8, col_W_int8.T) #
             dcol_q = self.quantizer.dequantize_dcol(dcol_int)
@@ -169,16 +156,13 @@ class Conv:
 
 
 class FC:
-    def __init__(self, input_channel, output_channel, optimizer=SGD()):
-        self.W = self.initialize_weights(input_channel, output_channel)
-        self.b = mypy.zeros((1, output_channel))
-        self.optimizer = optimizer
-
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
         self.x = None
-
-    def initialize_weights(self, input_channel, output_channel):
-        a = mypy.sqrt(2 / input_channel)
-        return mypy.random.normal(loc=0, scale=a, size=(input_channel, output_channel))
+        
+        self.dW = None
+        self.db = None
 
     def forward(self, x):
         self.x = mypy.squeeze(x)
@@ -186,31 +170,29 @@ class FC:
         return out
 
     def backward(self, dout):
-        dW = mypy.dot(self.x.T, dout)
-        db = mypy.sum(dout, axis=0, keepdims=True)
+        self.dW = mypy.dot(self.x.T, dout)
+        self.db = mypy.sum(dout, axis=0, keepdims=True)
         dx = mypy.dot(dout, self.W.T)
-
-        self.optimizer.update(self.W, dW)
-        self.optimizer.update(self.b, db)
 
         return dx
     
 
 class BatchNorm:
-    def __init__(self, input_size, optimizer, epsilon=1e-5, momentum=0.9):
-        self.epsilon = epsilon
+    def __init__(self, gamma, beta, running_mean, running_var, epsilon=1e-5, momentum=0.9):
         self.momentum = momentum
-        self.input_size = input_size
-        self.gamma = mypy.ones((1, input_size, 1, 1))  # 初始化为1
-        self.beta = mypy.zeros((1, input_size, 1, 1))  # 初始化为0
-        self.running_mean = mypy.zeros((1, input_size, 1, 1))
-        self.running_var = mypy.zeros((1, input_size, 1, 1))
-        self.optimizer = optimizer
+        self.gamma = gamma
+        self.beta = beta
+        self.running_mean = running_mean
+        self.running_var = running_var
+        self.epsilon = epsilon
 
         self.x = None
         self.mean = None
         self.var = None
         self.x_normalized = None
+
+        self.dgamma = None
+        self.dbeta = None
 
     def forward(self, x, train_mode = True):
         self.x = x
@@ -232,17 +214,14 @@ class BatchNorm:
     def backward(self, dout):
         batch_size = dout.shape[0]
 
-        dgamma = mypy.sum(dout * self.x_normalized, axis=(0, 2, 3), keepdims=True)
-        dbeta = mypy.sum(dout, axis=(0, 2, 3), keepdims=True)
+        # 计算梯度
+        self.dgamma = mypy.sum(dout * self.x_normalized, axis=(0, 2, 3), keepdims=True)
+        self.dbeta = mypy.sum(dout, axis=(0, 2, 3), keepdims=True)
         dx_normalized = dout * self.gamma
 
         dvar = mypy.sum(dx_normalized * (self.x - self.mean) * -0.5 * (self.var + self.epsilon) ** (-1.5), axis=0)
         dmean = mypy.sum(dx_normalized * -1 / mypy.sqrt(self.var + self.epsilon), axis=0) + dvar * mypy.sum(-2 * (self.x - self.mean), axis=0) / batch_size
         dx = dx_normalized / mypy.sqrt(self.var + self.epsilon) + dvar * 2 * (self.x - self.mean) / batch_size + dmean / batch_size
-
-        # 更新 gamma 和 beta
-        self.optimizer.update(self.gamma, dgamma)
-        self.optimizer.update(self.beta, dbeta)
 
         return dx
 
--- models/vgg11.py
+++ models/vgg11.py
@@ -7,14 +7,9 @@ class VGG11:
     def __init__(self, optimizer, quant_mode=QuantMode.FullPrecision, class_num=10, load_file = None):
         self.optimizer = optimizer
         self.quant_mode = quant_mode
-        self.layers = self.make_layers(cfg, input_channel = 3, batch_norm=True, class_num = class_num)
-        self.softmax = SoftmaxWithLoss()
-
         self.params = {}
-        if load_file:
-            self.load_params(load_file)
-        else:
-            self.init_params()
+        self.layers = self.make_layers(cfg, input_channel = 3, batch_norm=True, class_num = class_num, file_name = load_file)
+        self.softmax = SoftmaxWithLoss()
 
     def predict(self, x):
         for layer in self.layers:
@@ -35,6 +30,7 @@ class VGG11:
     
     def backward(self):
         dout = self.softmax.backward()
+        gradient = {}
 
         for layer in reversed(self.layers):
             dout = layer.backward(dout)
@@ -44,105 +40,83 @@ class VGG11:
         return dout
 
     # batch_norm 从一个隐藏层获取输出并在将它们作为下一个隐藏层的输入传递之前对其进行"标准化"
-    def make_layers(self, cfg, batch_norm=False, input_channel = 3, class_num = 10):
-        layers = []   
-        for l in cfg:
-            if l == 'M':
-                layers += [Pooling(2, 2)]
-                continue
-            layers += [Conv(optimizer=self.optimizer, 
-                            input_channel=input_channel, output_channel=l, 
-                            kernel_size=3, pad=1, 
-                            quant_mode=self.quant_mode)]
-            if batch_norm == True:
-                layers += [BatchNorm(optimizer=self.optimizer, input_size=l)]
-            layers += [Relu()]
-            input_channel = l
+    def make_layers(self, cfg, batch_norm=False, input_channel = 3, class_num = 10, file_name = None):
+        layers = []
 
-        layers += [FC(512, 4096, optimizer=self.optimizer), 
-                   FC(4096, 4096, optimizer=self.optimizer), 
-                   FC(4096, class_num, optimizer=self.optimizer)]
+        # 有预训练模型
+        if file_name:
+            with open(file_name, 'rb') as f:
+                self.params = pickle.load(f)
+
+            for l in cfg:
+                if l == 'M':
+                    layers += [Pooling(2, 2)]
+                    continue
+
+                layers += [Conv(W=self.params['Conv_W_' + str(len(layers))],
+                                b=self.params['Conv_b_' + str(len(layers))],
+                                input_channel=input_channel, output_channel=l, 
+                                kernel_size=3, pad=1, 
+                                quant_mode=self.quant_mode)]
+                if batch_norm == True:
+                    layers += [BatchNorm(
+                        gamma=self.params['BN_Gamma_' + str(len(layers))],
+                        beta=self.params['BN_Beta_' + str(len(layers))],
+                        running_mean=mypy.zeros((1, l, 1, 1)),
+                        running_var=mypy.zeros((1, l, 1, 1))
+                    )]
+                layers += [Relu()]
+                input_channel = l
+
+            layers += [FC(self.params['FC_W_' + str(len(layers))], self.params['FC_b_' + str(len(layers))]), 
+                       FC(self.params['FC_W_' + str(len(layers)+1)], self.params['FC_b_' + str(len(layers)+1)]),
+                       FC(self.params['FC_W_' + str(len(layers)+2)], self.params['FC_b_' + str(len(layers)+2)])]
+        
+        # 无预训练模型
+        else:
+            for l in cfg:
+                if l == 'M':
+                    layers += [Pooling(2, 2)]
+                    continue
+
+                self.params['Conv_W_' + str(len(layers))] = mypy.random.normal(loc=0, scale=0.01, size=(l, input_channel, 3, 3))
+                self.params['Conv_b_' + str(len(layers))] = mypy.zeros(l, dtype=float)
+                layers += [Conv(W=self.params['Conv_W_' + str(len(layers))],
+                                b=self.params['Conv_b_' + str(len(layers))],
+                                optimizer=self.optimizer, 
+                                pad=1,
+                                quant_mode=self.quant_mode)]
+                if batch_norm == True:
+                    self.params['BN_Gamma_' + str(len(layers))] = mypy.ones((1, l, 1, 1))
+                    self.params['BN_Beta_' + str(len(layers))] = mypy.zeros((1, l, 1, 1))
+                    layers += [BatchNorm(
+                        gamma=self.params['BN_Gamma_' + str(len(layers))],
+                        beta=self.params['BN_Beta_' + str(len(layers))],
+                        running_mean=mypy.zeros((1, l, 1, 1)),
+                        running_var=mypy.zeros((1, l, 1, 1))
+                    )]
+                layers += [Relu()]
+                input_channel = l
+
+            layers += [FC(self.params['FC_W_' + str(len(layers))], self.params['FC_b_' + str(len(layers))]), 
+                       FC(self.params['FC_W_' + str(len(layers)+1)], self.params['FC_b_' + str(len(layers)+1)]),
+                       FC(self.params['FC_W_' + str(len(layers)+2)], self.params['FC_b_' + str(len(layers)+2)])]
+            
+            FC_channel = [512, 4096, 4096, class_num]
+            for i in range(len(FC_channel) - 1):
+                FC_in = FC_channel[i]
+                FC_out = FC_channel[i+1]
+                self.params['FC_W_' + str(len(layers)+i)] = mypy.random.normal(loc=0, scale=mypy.sqrt(2 / FC_in), size=(FC_in, FC_out))
+                self.params['FC_b_' + str(len(layers)+i)] = mypy.zeros((1, FC_out))
+                layers += [FC(W=self.params['FC_W_' + str(len(layers)+i)], 
+                              b=self.params['FC_b_' + str(len(layers)+i)])]
         
         return layers
     
     def save_params(self, file_name="result/model/vgg11.pkl"):
         hint("=======        Save Params        =======")
 
-        params = {}
-        for i, layer in enumerate(self.layers):
-            if isinstance(layer, Conv):
-                # print(type(layer))
-                params['Conv_W_' + str(i+1)] = layer.W
-                params['Conv_b_' + str(i+1)] = layer.b
-                # FullPrecision only save W, b
-                if self.quant_mode != QuantMode.FullPrecision:
-                    pass
-
-            elif isinstance(layer, BatchNorm):
-                # print(type(layer))
-                params['BN_Epsilon_' + str(i+1)] = layer.epsilon
-                params['BN_Gamma_' + str(i+1)] = layer.gamma
-                params['BN_Beta_' + str(i+1)] = layer.beta
-                params['BN_running_mean_' + str(i+1)] = layer.running_mean
-                params['BN_running_var_' + str(i+1)] = layer.running_var
-
-            elif isinstance(layer, FC):
-                # print(type(layer))
-                params['FC_W_' + str(i+1)] = layer.W
-                params['FC_b_' + str(i+1)] = layer.b
-
         with open(file_name, 'wb') as f:
-            pickle.dump(params, f)
+            pickle.dump(self.params, f)
 
         hint("=======    Success Save Params    =======")
-
-    def load_params(self, file_name="result/model/vgg11.pkl"):
-        hint("=======        Load Params        =======")
-
-        with open(file_name, 'rb') as f:
-            self.params = pickle.load(f)
-
-        for i, layer in enumerate(self.layers):
-            if isinstance(layer, Conv):
-                layer.W = self.params['Conv_W_' + str(i+1)]
-                layer.b = self.params['Conv_b_' + str(i+1)]
-                # FullPrecision only save W, b
-                if self.quant_mode != QuantMode.FullPrecision:
-                    pass
-
-            elif isinstance(layer, BatchNorm):
-                layer.epsilon = self.params['BN_Epsilon_' + str(i+1)]
-                layer.gamma = self.params['BN_Gamma_' + str(i+1)]
-                layer.beta = self.params['BN_Beta_' + str(i+1)]
-                layer.running_mean = self.params['BN_running_mean_' + str(i+1)]
-                layer.running_var = self.params['BN_running_var_' + str(i+1)]
-
-            elif isinstance(layer, FC):
-                layer.W = self.params['FC_W_' + str(i+1)]
-                layer.b = self.params['FC_b_' + str(i+1)]
-        
-        hint("=======    Success Load Params    =======")
-
-    def init_params(self, file_name="result/model/vgg11.pkl"):
-        hint("=======        Init Params        =======")
-
-        for i, layer in enumerate(self.layers):
-            if isinstance(layer, Conv):
-                layer.W = self.params['Conv_W_' + str(i+1)]
-                layer.b = self.params['Conv_b_' + str(i+1)]
-                # FullPrecision only save W, b
-                if self.quant_mode != QuantMode.FullPrecision:
-                    pass
-
-            elif isinstance(layer, BatchNorm):
-                layer.epsilon = self.params['BN_Epsilon_' + str(i+1)]
-                layer.gamma = self.params['BN_Gamma_' + str(i+1)]
-                layer.beta = self.params['BN_Beta_' + str(i+1)]
-                layer.running_mean = self.params['BN_running_mean_' + str(i+1)]
-                layer.running_var = self.params['BN_running_var_' + str(i+1)]
-
-            elif isinstance(layer, FC):
-                layer.W = self.params['FC_W_' + str(i+1)]
-                layer.b = self.params['FC_b_' + str(i+1)]
-        
-        hint("=======    Success Init Params    =======")
